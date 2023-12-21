d9f470d089d14ff1ab793d89c5c3a0b8608fe97f
iamvinalsharma <152777568+iamvinalsharma@users.noreply.github.com>
2023-12-22 00:25:46 +0530

    Completed 01-requestcount.js

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -1,5 +1,3 @@
-const request = require('supertest');
-const assert = require('assert');
 const express = require('express');
 
 const app = express();
@@ -8,7 +6,14 @@ let requestCount = 0;
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
-// requestCount variable
+// requestCount variable.
+
+function requestsCountMiddleware(req, res, next) {
+  requestCount = requestCount + 1;
+  next();
+}
+
+app.use(requestsCountMiddleware);
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -23,3 +28,28 @@ app.get('/requestCount', function(req, res) {
 });
 
 module.exports = app;
+
+// Testing the code:
+
+// 18:41 $ npx jest ./tests/01-requestcount.spec.js 
+//  PASS  tests/01-requestcount.spec.js
+//   GET /user
+//     ✓ One request responds with 1 (40 ms)
+//     ✓ 10 more requests log 12 (5 ms)
+
+// --------------------|---------|----------|---------|---------|-------------------
+// File                | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s 
+// --------------------|---------|----------|---------|---------|-------------------
+// All files           |   94.11 |      100 |     100 |   94.11 |                   
+//  01-requestcount.js |   94.11 |      100 |     100 |   94.11 | 21,25             
+// --------------------|---------|----------|---------|---------|-------------------
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.605 s, estimated 1 s
+// Ran all test suites matching /.\/tests\/01-requestcount.spec.js/i.
+// ReferenceError: done is not defined
+//     at /Users/vinalsharma/Desktop/DEV/assignments-master_Week3/week-3/01-middlewares/tests/01-requestcount.spec.js:17:7
+//     at processTicksAndRejections (node:internal/process/task_queues:95:5)
+
+// There is an error in ./tests/01-requestcount.spec.js, but that can be ignored for now. This file is all pass.
